bf86bb5d95d3648d1111f4be3bc2c1747f301c71
liubom <liubom@gmail.com>
2021-04-16 20:29:55 +0300

    Comments...

--- src/components/Article/AddComment/AddComment.js
+++ src/components/Article/AddComment/AddComment.js
@@ -6,11 +6,49 @@ const AddComment = (props) => {
         return null;
     }
 
+    const sendComment = (val) => {
+
+        if (val === '') {
+            return;
+        }
+
+        const maxChars = 500;
+
+        let ifArticleCommented = localStorage.getItem(`${props.aData.id}-articles-comments`);
+        console.log(ifArticleCommented);
+
+        if (!ifArticleCommented) {
+
+            localStorage.setItem(`${props.aData.id}-articles-comments`, "[]");
+
+            const arrayOfComments = JSON.parse(localStorage.getItem(`${props.aData.id}-articles-comments`));
+
+            arrayOfComments.push({id: props.aData.id, user: localStorage.getItem('user'), comment: val});
+
+            localStorage.setItem(`${props.aData.id}-articles-comments`, JSON.stringify([...arrayOfComments]));
+        } else {
+            const arrayOfComments = JSON.parse(localStorage.getItem(`${props.aData.id}-articles-comments`));
+
+            arrayOfComments.push({id: props.aData.id, user: localStorage.getItem('user'), comment: val});
+
+            localStorage.setItem(`${props.aData.id}-articles-comments`, JSON.stringify([...arrayOfComments]));
+        }
+    }
 
     return (
         <div className='add_comment_modal'>
             <div className='add_comment_modal_content'>
                 <h3>{props.aData.title}</h3>
+                <div className='add_comments'>
+                    <textarea onChange={(e) => {
+                        console.log(e.currentTarget.value.length);
+                    }} className='comment_text_input'/>
+                    <button className='send_comment' onClick={(e) => {
+                        sendComment(e.currentTarget.previousSibling.value);
+                        e.currentTarget.previousSibling.value = '';
+                    }}>SEND COMMENT
+                    </button>
+                </div>
                 <div>
                     <button className='close_add_comments' onClick={props.toggleModalAdd}>CLOSE</button>
                 </div>
--- src/components/Article/ViewComments/ViewComments.js
+++ src/components/Article/ViewComments/ViewComments.js
@@ -11,6 +11,10 @@ const ViewComments = (props) => {
         <div className='comments_modal'>
             <div className='comments_modal_content'>
                 <h3>{props.aData.title}</h3>
+                <h4>Comments</h4>
+                <div className='comments'>
+                    ALL COMMENTS HERE...
+                </div>
                 <div>
                     <button className='close_comments' onClick={props.toggleModalView}>CLOSE</button>
                 </div>
